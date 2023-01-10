0f553ab813bb85722e00367d46dc6350fbe8c4ab
noman318 <shaikhmohdnoman612@gmail.com>
2023-01-10 16:21:07 +0530

    Issue fixed in Routes

--- src/components/AuthRoute.tsx
+++ src/components/AuthRoute.tsx
@@ -1,32 +1,31 @@
-import React, { useState,PropsWithChildren, useEffect } from 'react'
-import {getAuth, onAuthStateChanged} from 'firebase/auth'
+import React, { useEffect, useState,PropsWithChildren } from 'react';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 
-// interface AuthPageProps {children}
- 
+export interface IAuthRouteProps {}
+
 const AuthRoute: React.FunctionComponent<PropsWithChildren> = (props) => {
-    const {children} = props
+    const { children } = props;
     const auth = getAuth();
     const navigate = useNavigate();
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const [loading, setLoading] = useState(false);
-    
-    const AuthCheck = onAuthStateChanged(auth,(user)=>{
-        if(user){
-            setLoading(false)
-        }else{
-            navigate('/login')
-            console.log('unauthorized')
-        }
-    })
-    useEffect(()=>{
-        AuthCheck()
-        return () => AuthCheck()
-    },[auth,AuthCheck,navigate])
-    //  if(loading) return <p>loading ...</p>
-    return ( 
-        <>{children}</>
-     );
-}
- 
+
+    useEffect(() => {
+        const AuthCheck = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setLoading(false);
+            } else {
+                console.log('unauthorized');
+                navigate('/login');
+            }
+        });
+
+        return () => AuthCheck();
+    }, [auth,navigate]);
+
+    if (loading) return <p>loading ...</p>;
+
+    return <>{children}</>;
+};
+
 export default AuthRoute;
